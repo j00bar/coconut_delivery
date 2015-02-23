528f5abed6b7e3bff2b27d30e2f49ae22856d200
Joshua "jag" Ginsberg <jag@flowtheory.net>
2015-02-22 22:45:07 -0500

    Initial implementation - passes unit tests.

--- jetstream.py
+++ jetstream.py
@@ -9,6 +9,7 @@ class JetStream(object):
 
     # A dictionary of {start: [{'finish': finish, 'cost': cost}]}
     jetstreams = {}
+    padded = False
 
     def __init__(self, base_cost):
         self.base_cost = base_cost
@@ -37,3 +38,47 @@ class JetStream(object):
         logger.debug('Parsed jetstream map is %s', js.jetstreams)
         ifs.close()
         return js
+
+    def pad_jetstream_with_base_steps(self):
+        """The base jetstream data does not include the steps possible with
+        traveling one mile at the base cost. We need to pad the jetstream data
+        with those one-mile-steps."""
+        if not self.path_length or not self.jetstreams:
+            raise RuntimeError('Please initialize the jetstream data first.')
+        if self.padded:
+            # No need to do this twice.
+            return
+        for i in xrange(self.path_length):
+            self.jetstreams.setdefault(i, []).append(
+                {'finish': i+1, 'cost': self.base_cost, 'no_stream': True}
+            )
+
+    def find_optimal_path(self):
+        self.pad_jetstream_with_base_steps()
+        # Our upper bound is ignoring all jetstreams and just going one mile
+        # at a time.
+        self.optimal_path_cost = self.path_length * self.base_cost
+        self.optimal_path = []
+
+        def take_a_step(current_place, jetstream_trail, running_cost):
+            # Termination clause
+            if current_place == self.path_length:
+                if running_cost < self.optimal_path_cost:
+                    # We've found a more efficient path! Yay us.
+                    # FIXME: NOT THREAD SAFE!!!!
+                    self.optimal_path_cost = running_cost
+                    self.optimal_path = jetstream_trail
+                return
+            optional_next_steps = self.jetstreams[current_place]
+            for step in optional_next_steps:
+                take_a_step(step['finish'],
+                            jetstream_trail
+                            if 'no_stream' in step
+                            else jetstream_trail + [(current_place,
+                                                     step['finish'])],
+                            running_cost + step['cost'])
+
+        # Kick off the pathfinder
+        take_a_step(0, [], 0)
+
+
